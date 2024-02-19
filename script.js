d83695cae3e7ad727d252e21d94bf568a48bcc7e
KasiaH <kah.gda@wp.pl>
2024-02-19 12:59:59 +0100

    a code review has been performed

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const search = document.querySelector('.search')
-const li = document.querySelectorAll('li')
+const search = document.querySelector('.search');
+const li = document.querySelectorAll('li');
 
 const searchEngine = e => {
-
     const text = e.target.value.toLowerCase();
 
     li.forEach(el => {
-
         if (el.textContent.toLocaleLowerCase().indexOf(text) !== -1) {
             el.style.display = 'block'
         } else {
-            el.style.display = 'none' 
+            el.style.display = 'none'
         }
     })
-}
+};
 
-search.addEventListener('keyup', searchEngine)
+search.addEventListener('keyup', searchEngine);
 
